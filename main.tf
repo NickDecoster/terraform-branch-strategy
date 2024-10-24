65715e9df723ee154d9868b030cf4203a935178e
NickDecoster <36927006+NickDecoster@users.noreply.github.com>
2024-10-24 13:18:59 +0200

    Dev (#3)
    * add virtual network + specify location germany
    
    * use region germanycentral
    
    * use region francecentral
    
    * use region polandcentral
    
    * rename vn
    
    * setup aks module (#2)
    
    * fix: broader address space
    
    * fixes

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+locals {
+  location = "polandcentral"
+}
+
 data "azurerm_resource_group" "rg-tfworkshops" {
   name = "Terraform-workshops"
+}
+
+resource "azurerm_virtual_network" "vn" {
+  address_space       = ["10.0.0.0/8"]
+  location            = local.location
+  name                = "multicloud-nick-dev-vn"
+  resource_group_name = data.azurerm_resource_group.rg-tfworkshops.name
+}
+
+module "aks" {
+  source               = "./modules/aks"
+  env_prefix           = var.env_prefix
+  prefix               = var.prefix
+  resource_group_name  = data.azurerm_resource_group.rg-tfworkshops.name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  location             = local.location
+
+  depends_on = [azurerm_virtual_network.vn]
 }
--- versions.tf
+++ versions.tf
@@ -1,19 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "4.6.0"
     }
     null = {
-      source = "hashicorp/null"
+      source  = "hashicorp/null"
       version = "3.2.3"
     }
   }
 
   backend "azurerm" {
-    resource_group_name  = "Terraform-workshops"                  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "axxesworkshoptfstates"                # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"                             # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    resource_group_name  = "Terraform-workshops"   # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "axxesworkshoptfstates" # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     # key                  = "lorenzo-dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
     use_azuread_auth = true
   }
