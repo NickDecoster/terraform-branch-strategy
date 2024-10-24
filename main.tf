5c088b91ea0b7885dc6229fc7697a52c0efecae2
NickDecoster <nick.decoster@gmail.com>
2024-10-24 11:43:57 +0200

    fix: broader address space

--- main.tf
+++ main.tf
@@ -7,16 +7,18 @@ data "azurerm_resource_group" "rg-tfworkshops" {
 }
 
 resource "azurerm_virtual_network" "vn" {
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/8"]
   location            = local.location
   name                = "multicloud-nick-dev-vn"
   resource_group_name = data.azurerm_resource_group.rg-tfworkshops.name
 }
 
 module "aks" {
-  source = "./modules/aks"
-  env_prefix = var.env_prefix
-  prefix = var.prefix
-  resource_group_name = data.azurerm_resource_group.rg-tfworkshops.name
+  source               = "./modules/aks"
+  env_prefix           = var.env_prefix
+  prefix               = var.prefix
+  resource_group_name  = data.azurerm_resource_group.rg-tfworkshops.name
   virtual_network_name = azurerm_virtual_network.vn.name
+
+  depends_on = [azurerm_virtual_network.vn]
 }
