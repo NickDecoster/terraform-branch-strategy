a8a17f40ca547831d3aae3b26caebb1e53a8deff
NickDecoster <nick.decoster@gmail.com>
2024-10-24 11:22:04 +0200

    rename vn

--- main.tf
+++ main.tf
@@ -9,6 +9,6 @@ data "azurerm_resource_group" "rg-tfworkshops" {
 resource "azurerm_virtual_network" "vn" {
   address_space       = ["10.0.0.0/16"]
   location            = local.location
-  name                = "terraform-muliticloud-nick-vn"
+  name                = "multicloud-nick-dev-vn"
   resource_group_name = data.azurerm_resource_group.rg-tfworkshops.name
 }
